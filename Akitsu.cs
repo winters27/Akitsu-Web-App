30da8290e992ed337a65df2a8dd98b150f73949e
winters27 <brandonwinters27@yahoo.com>
2026-04-09 13:18:10 -0700

    Include C# Source and native compiling documentation

---
+++ Akitsu.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AkitsuApp {
+    class Program {
+        [STAThread]
+        static void Main() {
+            string url = "https://miceapp.arbiterstudio.com/#/project/items";
+            int desiredWidth = 1340;
+            int desiredHeight = 1040;
+            string profileDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "AkitsuWebApp_Profile");
+
+            string edge64 = @"C:\Program Files\Microsoft\Edge\Application\msedge.exe";
+            string edge32 = @"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe";
+            string edgeExe = File.Exists(edge64) ? edge64 : (File.Exists(edge32) ? edge32 : "msedge.exe");
+
+            var wa = Screen.PrimaryScreen.WorkingArea;
+            int x = (int)Math.Round((wa.Width - desiredWidth) / 2.0) + wa.Left;
+            int y = (int)Math.Round((wa.Height - desiredHeight) / 2.0) + wa.Top;
+
+            string argsLine = "--app=\"" + url + "\" " +
+                              "--window-size=" + desiredWidth + "," + desiredHeight + " " +
+                              "--window-position=" + x + "," + y + " " +
+                              "--user-data-dir=\"" + profileDir + "\" " +
+                              "--disable-extensions " +
+                              "--disable-plugins " +
+                              "--disable-background-networking " +
+                              "--disable-sync " +
+                              "--disable-translate " +
+                              "--disable-default-apps " +
+                              "--disable-component-extensions-with-background-pages " +
+                              "--no-default-browser-check " +
+                              "--no-first-run " +
+                              "--disable-client-side-phishing-detection " +
+                              "--disable-features=IsolateOrigins,site-per-process";
+
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = edgeExe;
+            psi.Arguments = argsLine;
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = true;
+
+            try {
+                Process.Start(psi);
+            } catch (Exception ex) {
+                MessageBox.Show("Failed to launch Akitsu Web App.\n\n" + ex.Message, "Launch Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
